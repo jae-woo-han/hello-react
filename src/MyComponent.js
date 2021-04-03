0b68f8adf52c9140c44cc9f26335f7f75b1d76e4
jae-woo-han <40654123+jae-woo-han@users.noreply.github.com>
2021-04-03 23:10:29 +0900

    props into class

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -14,6 +14,13 @@ import PropTypes from 'prop-types';
 // };
 
 class MyComponent extends Component {
+  static defaultProps = {
+    name: '기본 이름',
+  };
+  static propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired,
+  };
   render() {
     const { name, favoriteNumber, children } = this.props; //비구조화 할당
     return (
@@ -28,13 +35,13 @@ class MyComponent extends Component {
   }
 }
 
-MyComponent.defaultProps = {
-  name: '기본 이름',
-};
+// MyComponent.defaultProps = {
+//   name: '기본 이름',
+// };
 
-MyComponent.propTypes = {
-  name: PropTypes.string,
-  favoriteNumber: PropTypes.number.isRequired,
-};
+// MyComponent.propTypes = {
+//   name: PropTypes.string,
+//   favoriteNumber: PropTypes.number.isRequired,
+// };
 
 export default MyComponent;
