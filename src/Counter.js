17ff10b45d7176e8c5faf880e1174c4cbf2e9157
jae-woo-han <40654123+jae-woo-han@users.noreply.github.com>
2021-04-04 00:12:37 +0900

    생성자 형식으로 state 등록

---
+++ src/Counter.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    //state의 초기 값 설정
+    this.state = {
+      number: 0,
+      fixedNumber : 0
+    };
+  }
+
+  render() {
+    const { number, fixedNumber } = this.state; //조회 할 때는 this.state로 조회
+    return (
+      <div>
+        <h1>{number}</h1>
+        <h2>바뀌지 않는 값 : {fixedNumber}</h2>
+        <button
+          onClick={() => {
+            this.setState({ number: number + 1 });
+          }}
+        >
+          +1
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
