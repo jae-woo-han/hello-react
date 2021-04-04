94c5098b089922372b1ca6d4782fea255f6daec5
jae-woo-han <40654123+jae-woo-han@users.noreply.github.com>
2021-04-04 23:43:40 +0900

    e.target.name form객체로 useState 쓰기

--- src/EventPracticeFuncComp.js
+++ src/EventPracticeFuncComp.js
@@ -1,39 +1,48 @@
 import React, { useState } from 'react';
 
 const EventPracticeFuncComp = () => {
+  const [form, setForm] = useState({
+    username: '',
+    message: ''
+  });
+  const {username, message} = form;
+  const onChange = (e) => {
+    const nextForm = {
+      ...form, // 기존의 form 객체 안에 있는 것들 가져온다(spread 연산자 검색)
+      [e.target.name]: e.target.value,
+    };
+    setForm(nextForm);
+  };
 
-    const [username, setUsername] = useState('');
-    const [message, setMessage] = useState('');
-    
-    const onChangeUsername = e => setUsername(e.target.value);
-    const onChangeMessage = e => setMessage(e.target.value);
-    const onClick = () =>{
-        alert(username + ' : ' + message);
-        setUsername('');
-        setMessage('');
+  const onClick = () => {
+    alert(username + ' : ' + message);
+    setForm({
+      username: '',
+      message: '',
+    });
+  };
+  const onKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      onClick();
     }
-    const onKeyPress = e =>{
-        if(e.key === 'Enter'){
-            onClick();
-        }
-    }
+  };
 
   return (
     <div>
       <h1>이벤트 연습</h1>
       <input
-        type='text'
-        name='username'
-        placeholder='사용자명'
+        type="text"
+        name="username"
+        placeholder="사용자명"
         value={username}
-        onChange={onChangeUsername}
+        onChange={onChange}
       />
       <input
-        type='text'
-        name='message'
-        placeholder='입력하세요'
+        type="text"
+        name="message"
+        placeholder="입력하세요"
         value={message}
-        onChange={onChangeMessage}
+        onChange={onChange}
         onKeyPress={onKeyPress}
       />
       <button onClick={onClick}>확인</button>
