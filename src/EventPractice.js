d2ed0d01829b59b7ab27e1ad2e03d695fc261b6c
jae-woo-han <40654123+jae-woo-han@users.noreply.github.com>
2021-04-04 22:59:54 +0900

    keypress

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class EventPractice extends Component {
   state = {
+    username: "",
     message: '',
   };
 
@@ -13,29 +14,43 @@ class EventPractice extends Component {
 
   handleChange=(e)=>{
       this.setState({
-          message: e.target.value
+          [e.target.name]: e.target.value
       });
   }
 
   handleClick=()=>{
-      alert(this.state.message);
+      alert(this.state.username + " : "+ this.state.message);
       this.setState({
+          username : "",
           message: ""
       });
   }
 
+  handleKeyPress = (e) => {
+      if(e.key === 'Enter'){
+          this.handleClick();
+      }
+  }
+
   render() {
-    const { message } = this.state;
+   
     return (
       <div>
         <h1>이벤트 연습</h1>
-        <h2>{message}</h2>
+        <input 
+            type='text'
+            name='username'
+            placeholder='사용자명'
+            value = {this.state.username}
+            onChange = {this.handleChange}
+        />
         <input
           type="text"
           name="message"
           placeholder="입력하세요"
           value={this.state.message}
           onChange={this.handleChange}
+          onKeyPress = {this.handleKeyPress}
         />
         <button
           onClick={this.handleClick}
