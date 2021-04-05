7e5fb4a7eedb5eb2ca6c9dfb751418becf03e30d
jae-woo-han <40654123+jae-woo-han@users.noreply.github.com>
2021-04-05 23:30:28 +0900

    부모컴포넌트에서 자식ref해서 쓰기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
 // const App = () => {
-//   return <ValidationSample />;
+//   return <ScrollBox />;
 // };
 
 class App extends Component {
   render() {
     return (
-      <ValidationSample/>
+      <div>
+        <ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>맨 밑으로</button>
+      </div>
     );
   }
 }
