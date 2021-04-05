f5eb256c5c9d8c0a69672f31762eea2e5b3d9434
jae-woo-han <40654123+jae-woo-han@users.noreply.github.com>
2021-04-05 23:19:48 +0900

    ref적용

--- src/ValidationSample.js
+++ src/ValidationSample.js
@@ -1,39 +1,47 @@
 import React, { Component } from 'react';
-import './ValidationSample.css'
+import './ValidationSample.css';
 
 class ValidationSample extends Component {
+  state = {
+    password: '',
+    clicked: false,
+    validated: false,
+  };
 
-    state = {
-        password: '',
-        clicked: false,
-        validated: false,
-    }
+  handleChange = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
 
-    handleChange = (e)=>{
-        this.setState({
-            password : e.target.value,
-        });
-    }
-
-    handleButtonClick = () => {
-        this.setState({
-            clicked: true,
-            validated: this.state.password === '0000',
-        });
-    } 
-    render() {
-        return (
-            <div>
-                <input
-                    type = "password"
-                    value = {this.state.password}
-                    onChange = {this.handleChange}
-                    className = {this.state.clicked ? (this.state.validated ? 'success' : 'failure'): ''}
-                />
-                <button onClick = {this.handleButtonClick}>검증하기</button>
-            </div>
-        );
-    }
+  handleButtonClick = () => {
+    this.setState({
+      clicked: true,
+      validated: this.state.password === '0000',
+    });
+    this.input.focus();
+  };
+  //버튼을 한번 눌렀을 때 포커스가 다시 input으로 넘어가게 작성해보자
+  render() {
+    return (
+      <div>
+        <input
+          ref={(ref) => this.input = ref}
+          type="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+          className={
+            this.state.clicked
+              ? this.state.validated
+                ? 'success'
+                : 'failure'
+              : ''
+          }
+        />
+        <button onClick={this.handleButtonClick}>검증하기</button>
+      </div>
+    );
+  }
 }
 
 export default ValidationSample;
