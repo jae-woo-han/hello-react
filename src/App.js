32ee92e0dd3081fea931d566e04dd0d3b87b41c3
jae-woo-han <40654123+jae-woo-han@users.noreply.github.com>
2021-04-08 22:44:58 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
-import React, { Component } from 'react';
-import LifeCycleSample from './LifeCycleSample';
-import ErrorBoundary from './ErrorBoundary';
+import React, { useState } from 'react';
+import Info from './hookSample/Info';
 
-// const App = () => {
-//   return <LifeCycleSample />;
-// };
+const App = () => {
+  const [visible, setVisible] = useState(false);
 
-//임의 색상 설정
-function getRandomColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
-class App extends Component {
-  state = {
-    color: '#000000',
-  };
-
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor(),
-    });
-  };
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>랜덤 색상</button>
-        <ErrorBoundary>
-          <LifeCycleSample color={this.state.color} />
-        </ErrorBoundary>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
+};
 
 export default App;
