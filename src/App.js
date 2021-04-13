36eac18b6770bc75a1ecaefd061cb980987fd0db
jae-woo-han <40654123+jae-woo-han@users.noreply.github.com>
2021-04-13 23:09:41 +0900

    useReducer, useMemo 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Counter from './hookSample/Counter';
+import Average from './hookSample/Average';
 
 const App = () => {
-  return <Counter />;
+  return <Average />;
 };
 
 export default App;
--- src/hookSample/Info.js
+++ src/hookSample/Info.js
@@ -1,30 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useReducer } from 'react';
+
+//리듀서 적용
+function reducer(state, action) {
+  //name에 적을 경우 name은 value를 받고 나머지는 구조분해 할당을 통해 현재 상태를 그대로 반환한다.
+  return {
+    ...state,//스테이트 전체할당 구조분해할당
+    [action.name]: action.value,//action으로 e.target이 넘어온다. 
+  };
+}
 
 const Info = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
-
-  useEffect(() => {
-    console.log('렌더링 완료');
-    console.log(name);
-    return () => {
-      console.log('cleanup');
-      console.log(name);
-    };
+  const [state, dispatch] = useReducer(reducer, {
+    name: '',
+    nickname: '',
   });
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
+  const { name, nickname } = state;
+
+  const onChange = (e) => {
+    dispatch(e.target);
   };
 
-  const onChangeNickName = (e) => {
-    setNickname(e.target.value);
-  };
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickName} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
